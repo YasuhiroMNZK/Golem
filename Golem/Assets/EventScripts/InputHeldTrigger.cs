c2a06c23bf4d30f38d737915ff5a56a709ac9981
YasuhiroMNZK <wei990630@gmail.com>
2025-12-13 23:35:43 +0900

    commit

---
+++ Golem/Assets/EventScripts/InputHeldTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InputHeldTrigger : TriggerBase
+{
+    [SerializeField] private string keyname;
+
+    // 離したときに実行するイベント
+    [SerializeField] private UnityEvent releaseAction;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 押した瞬間
+        if (Input.GetButton(keyname))
+        {
+            if (action != null)
+            {
+                action.Invoke();
+            }
+        }
+
+        // 離した瞬間
+        if (Input.GetButtonUp(keyname))
+        {
+            if (releaseAction != null)
+            {
+                releaseAction.Invoke();
+            }
+        }
+    }
+}
