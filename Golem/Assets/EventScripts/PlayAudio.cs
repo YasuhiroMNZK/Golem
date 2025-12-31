4238d23ae4d3056abf0955b73d33cb555dfa99c8
Sae Miyazaki <im0266hx@ed.ritsumei.ac.jp>
2025-12-31 16:04:44 +0900

    1231

--- Golem/Assets/EventScripts/PlayAudio.cs
+++ Golem/Assets/EventScripts/PlayAudio.cs
@@ -3,39 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-[RequireComponent(typeof(AudioSource))]
 public class PlayAudio : MonoBehaviour, ActionBase
 {
     AudioSource audiosource;
     private Coroutine intervalPlayCoroutine;
     
     [SerializeField] private float interval = 1.0f;
+    [SerializeField] private int loopCount = 5;
 
     private void Start()
     {
-        audiosource = GetComponent<AudioSource>();
+        InitializeAudioSource();
     }
+
+    private void InitializeAudioSource()
+    {
+        if (audiosource == null)
+        {
+            audiosource = GetComponent<AudioSource>();
+        }
+    }
+
     public void Action()
     {
-        audiosource.Play();
+        InitializeAudioSource();
+        if (audiosource != null)
+        {
+            audiosource.Play();
+        }
     }
 
     //シーンが変わっても音を鳴らし続ける
     public void KeepAction()
     {
-        DontDestroyOnLoad(gameObject);
-        audiosource.Play();
-        Destroy(gameObject, audiosource.clip.length);
+        InitializeAudioSource();
+        if (audiosource != null)
+        {
+            DontDestroyOnLoad(gameObject);
+            audiosource.Play();
+            Destroy(gameObject, audiosource.clip.length);
+        }
     }
 
     /// 一定間隔でSEを鳴らす
     public void Play()
     {
+        InitializeAudioSource();
+        if (audiosource == null)
+        {
+            Debug.LogWarning("AudioSource component not found on " + gameObject.name);
+            return;
+        }
+        
         if (intervalPlayCoroutine != null)
         {
             StopCoroutine(intervalPlayCoroutine);
         }
-        intervalPlayCoroutine = StartCoroutine(PlayIntervalCoroutine(interval));
+        intervalPlayCoroutine = StartCoroutine(PlayIntervalCoroutine(interval, loopCount));
     }
 
     /// 一定間隔でのSE再生を停止
@@ -48,13 +72,19 @@ public class PlayAudio : MonoBehaviour, ActionBase
         }
     }
 
-    // 一定間隔でSEを鳴らすコルーチン
-    private IEnumerator PlayIntervalCoroutine(float interval)
+    // 一定間隔でSEを鳴らすコルーチン（指定回数）
+    private IEnumerator PlayIntervalCoroutine(float interval, int loopCount)
     {
-        while (true)
+        for (int i = 0; i < loopCount; i++)
         {
-            audiosource.Play();
-            yield return new WaitForSeconds(interval);
+            if (audiosource != null)
+            {
+                audiosource.Play();
+            }
+            if (i < loopCount - 1) // 最後の再生後は待機しない
+            {
+                yield return new WaitForSecondsRealtime(interval);
+            }
         }
     }
 }
--- Golem/Assets/EventScripts/SaeScripts/SENPCVoice.cs
+++ Golem/Assets/EventScripts/SaeScripts/SENPCVoice.cs
@@ -1,16 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SENPCVoice : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    AudioSource audiosource;
+    private Coroutine intervalPlayCoroutine;
+    
+    [SerializeField] private float interval = 1.0f;
+    [SerializeField] private int loopCount = 5;
+
+    private void Start()
     {
-        
+        audiosource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    /// 一定間隔でSEを鳴らす
+    public void Play()
     {
-        
+        if (intervalPlayCoroutine != null)
+        {
+            StopCoroutine(intervalPlayCoroutine);
+        }
+        intervalPlayCoroutine = StartCoroutine(PlayIntervalCoroutine(interval, loopCount));
+    }
+
+    /// 一定間隔でのSE再生を停止
+    public void Stop()
+    {
+        if (intervalPlayCoroutine != null)
+        {
+            StopCoroutine(intervalPlayCoroutine);
+            intervalPlayCoroutine = null;
+        }
+    }
+
+    // 一定間隔でSEを鳴らすコルーチン（指定回数）
+    private IEnumerator PlayIntervalCoroutine(float interval, int loopCount)
+    {
+        for (int i = 0; i < loopCount; i++)
+        {
+            audiosource.Play();
+            if (i < loopCount - 1) // 最後の再生後は待機しない
+            {
+                yield return new WaitForSecondsRealtime(interval);
+            }
+        }
     }
 }
